c8468b2bcf5bd8a608b54f816a40a2bfee52c326
Brennen Green <brennengreen@outlook.com>
2020-05-26 12:51:36 -0400

    cards

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 1. Create an element in index.pug that will contain all the tweets - Joshua
 	* Use appropriate sizing I'm thinking we should be able to display 15 tweets
 	* Maybe look up similar examples
-2. Integrate the javascript twitter api to load tweets on page load
+2. Integrate the javascript twitter api to load tweets on page load - Brennen
 3. Figure out how to update the page in real time nicely (should be done after first two)
-4. Create an element that will EMBED the most relevant trending tweet
+4. Create an element that will EMBED the most relevant trending tweet - Joshua
 	* This element should have its own .pug file and then be included into index.pug
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
+var Twitter = require('twitter')
+
+// TODO: Move to env variables
+class Tweet {
+  constructor(author, msg) {
+    this.author = author;
+    this.message = msg;
+  }
+}
+let Tweets = [];
+let Topic = 'tennessee';
+var client = new Twitter ({
+  consumer_key: 'E2u0Wq5PKgDQgN3MsVVW5hJNb',
+  consumer_secret: 'UuLZXf4eP2B0Ujm4jdqLSpfOZ7esxxdr32XQDsVEllMXV6THdl',
+  access_token_key: '1263476735468810242-Npy4O7ec8G7v4758odM6oQm18rYPYF',
+  access_token_secret: '2nHnbt3jP3FhWOR4Ri50SAZM6jto4MmuOjT3gPAXqndcM'
+  //bearer_token: 'AAAAAAAAAAAAAAAAAAAAANVHEgEAAAAASRUZaKwErYfGvALqmSffLpX01LY%3D41Nbk7Huo6P4rDFcVEClkYngqHMV63nqCAAatFOu6FAMkhGD5G'
+})
+
+var stream = client.stream('statuses/filter', {track: Topic});
+stream.on('data', function(event) {
+  Tweets.unshift(new Tweet(event.user.name, event.text));
+  if (Tweets.length > 10) { Tweets.pop(); }
+})
+
+stream.on('error', function(error) {
+  console.log(error)
+  throw error;
+});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Com Tweets Test' });
+  res.render('index', { title: 'Com Tweet Filter', tweets: Tweets, filter_topic: Topic });
+});
+
+router.post('/filter', function(req, res, next) {
+  res.render('index', { title: 'Com Tweet Filter', tweets: Tweets, filter_topic: Topic });
 });
 
 module.exports = router;
